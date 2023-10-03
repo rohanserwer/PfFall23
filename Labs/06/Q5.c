eb603241f50bf5cfd4daa205c90d46487a213267
Rohan Serwer <143876943+rohanserwer@users.noreply.github.com>
2023-10-03 10:25:18 +0500

    Update Q5.c

--- Labs/06/Q5.c
+++ Labs/06/Q5.c
@@ -12,13 +12,13 @@ int main() {
     for (i = 0; i <= 6; i++) {
         // If i is even, repeat i times
         if (i % 2 == 0) {
-            for (j = 0; j < i; j++) {
+            for (j = 0; j < 4; j++) {
                 printf("%d ", i);
             }
         }
         // If i is odd, print i once
         else {
-            printf("%d", i);
+            printf("%d %d", i, i);
         }
 
         printf("\n");
